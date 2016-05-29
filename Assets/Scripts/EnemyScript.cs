642978865bcc8e8d26f355d835260722cfa175e9
Atmajr <jratma@gmail.com>
2016-05-29 01:37:00 -0400

    - disable enemy activity until the camera can view them - restrict player movement to camera view

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,34 +1,83 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class EnemyScript : MonoBehaviour {
-	/// <summary>
-	/// Enemy generic behavior
-	/// </summary>
-
+/// <summary>
+/// Enemy generic behavior
+/// </summary>
+public class EnemyScript : MonoBehaviour
+{
+	private bool hasSpawn;
+	private MoveScript moveScript;
 	private WeaponScript[] weapons;
 
-	void Awake () {
-		//retrieve the weapon only once
+	void Awake()
+	{
+		// Retrieve the weapon only once
 		weapons = GetComponentsInChildren<WeaponScript>();
 
+		// Retrieve scripts to disable when not spawn
+		moveScript = GetComponent<MoveScript>();
 	}
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	// 1 - Disable everything
+	void Start()
+	{
+		hasSpawn = false;
 
+		// Disable everything
+		// -- collider
+		GetComponent<Collider2D>().enabled = false;
+		// -- Moving
+		moveScript.enabled = false;
+		// -- Shooting
 		foreach (WeaponScript weapon in weapons)
 		{
-			//auto-fire
-			if (weapon != null && weapon.CanAttack)
+			weapon.enabled = false;
+		}
+	}
+
+	void Update()
+	{
+		// 2 - Check if the enemy has spawned.
+		if (hasSpawn == false)
+		{
+			if (GetComponent<Renderer>().IsVisibleFrom(Camera.main))
 			{
-				weapon.Attack(true);
+				Spawn();
 			}
+		}
+		else
+		{
+			// Auto-fire
+			foreach (WeaponScript weapon in weapons)
+			{
+				if (weapon != null && weapon.enabled && weapon.CanAttack)
+				{
+					weapon.Attack(true);
+				}
+			}
+
+			// 4 - Out of the camera ? Destroy the game object.
+			if (GetComponent<Renderer>().IsVisibleFrom(Camera.main) == false)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+
+	// 3 - Activate itself.
+	private void Spawn()
+	{
+		hasSpawn = true;
+
+		// Enable everything
+		// -- Collider
+		GetComponent<Collider2D>().enabled = true;
+		// -- Moving
+		moveScript.enabled = true;
+		// -- Shooting
+		foreach (WeaponScript weapon in weapons)
+		{
+			weapon.enabled = true;
 		}
 	}
 }
