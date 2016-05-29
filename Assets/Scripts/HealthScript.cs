1a66a7bf1e19c3d9117f4226f606f564d3e00633
Atmajr <jratma@gmail.com>
2016-05-29 05:54:08 -0400

    -health script changes missing from last commit for some reason

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -21,6 +21,9 @@ public class HealthScript : MonoBehaviour {
 
 		if (hp <= 0)
 		{
+			//Particle explosion
+			SpecialEffectsHelper.Instance.Explosion(transform.position);
+
 			//Dead
 			Destroy(gameObject);
 		}
